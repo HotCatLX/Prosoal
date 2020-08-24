1e103ff60152b65352addf299476b9189a934d1a
liuxiao <18389677071@163.com>
2020-08-25 01:23:02 +0800

    first build
    first build

---
+++ ProposalDaisakusen/ProposalDaisakusen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProposalDaisakusen
+//
+//  Created by suckerl on 2020/8/25.
+//  Copyright © 2020 suckerl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
